18ab789c333b796c83e1745dd64e6064cfe58c56
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air.local>
2023-07-22 08:43:03 +0100

    uses config file to set constants

--- main.py
+++ main.py
@@ -14,23 +14,25 @@ def add_rectangular_profile_box_to_face(profiles: List[Any], face):
         bbx_frontal_face = profile.get("bbx_frontal_face")
         if len(bbx_frontal_face) <= 0:
             continue
-        x0, y0,x1, y1 = bbx_frontal_face
-        rectangle_img = cv2.rectangle(face, (x0,y0),(x1,y1), (0,255,0),2)
+        x0, y0, x1, y1 = bbx_frontal_face
+        rectangle_img = cv2.rectangle(face, (x0, y0), (x1, y1), (0, 255, 0), 2)
         index = 0
         for key, val in profile.items():
             try:
-                cv2.putText(rectangle_img, 
-                            f"{key}: {val}" , (x0, y0-STEP-10*index), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 
-                            FONT_SIZE, 
-                            (0,255,0), TEXT_THICKNESS)
+                cv2.putText(
+                    rectangle_img,
+                    f"{key}: {val}",
+                    (x0, y0 - STEP - 10 * index),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    FONT_SIZE,
+                    (0, 255, 0),
+                    TEXT_THICKNESS,
+                )
                 index += 1
             except Exception as e:
                 print(e)
-            
-        return rectangle_img
 
-            
+        return rectangle_img
 
 
 while True:
@@ -38,15 +40,19 @@ while True:
     ret, frame = camera.read()
     frame = imutils.resize(frame, width=720)
 
-    end_time = time.time() - start_time    
-    FPS = 1/end_time
+    end_time = time.time() - start_time
+    FPS = 1 / end_time
 
-    cv2.putText(frame,
-                f"FPS: {round(FPS,3)}",
-                (10,50),
-                cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),
-                2)
-    cv2.imshow('Face info',frame)
+    cv2.putText(
+        frame,
+        f"FPS: {round(FPS,3)}",
+        (10, 50),
+        cv2.FONT_HERSHEY_COMPLEX,
+        1,
+        (0, 0, 255),
+        2,
+    )
+    cv2.imshow("Face info", frame)
 
-    if cv2.waitKey(1) &0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
