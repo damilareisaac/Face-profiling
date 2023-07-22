88a12b134e0a181e7de02a36591e8ada5813696d
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air.local>
2023-07-22 15:34:19 +0100

    Refactor transform image array, created a util file for all face transform operations

--- models/age_model.py
+++ models/age_model.py
@@ -1,9 +1,8 @@
 from deepface.basemodels import VGGFace
-from keras.preprocessing import image
 from keras.models import Model
 from keras.layers import Convolution2D, Flatten, Activation
 import numpy as np
-import cv2
+from .utils import transform_face_array
 
 from config import AGE_MODEL_WEIGHT_PATH
 
@@ -30,7 +29,7 @@ class AgeModel:
         return age_model
 
     def predict_age(self, face_image):
-        image_preprocesing = self.transform_face_array2age_face(face_image)
+        image_preprocesing = transform_face_array(face_image)
         age_predictions = self.model.predict(image_preprocesing)[0, :]
         result_age = self.find_apparent_age(age_predictions)
         return result_age
@@ -38,16 +37,3 @@ class AgeModel:
     def find_apparent_age(self, age_predictions):
         apparent_age = np.sum(age_predictions * self.output_indexes)
         return apparent_age
-
-    def transform_face_array2age_face(
-        self, face_array, grayscale=False, target_size=(224, 224)
-    ):
-        detected_face = face_array
-        if grayscale == True:
-            detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)
-        detected_face = cv2.resize(detected_face, target_size)
-        img_pixels = image.img_to_array(detected_face)
-        img_pixels = np.expand_dims(img_pixels, axis=0)
-        # normalize input in [0, 1]
-        img_pixels /= 255
-        return img_pixels
