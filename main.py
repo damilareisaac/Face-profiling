be2c746f7b5f28205bf80659b2fadd74d1219be7
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air.local>
2023-07-22 09:25:26 +0100

    Recognise frontal face and add box dimension of frontal face to profile rectangular box

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Any, List
+import face_recognition
 import cv2
 import imutils
 import time
+import numpy as np
 
 from config import FONT_SIZE, STEP, TEXT_THICKNESS
 
@@ -9,18 +11,34 @@ cv2.namedWindow("Image Profile")
 camera = cv2.VideoCapture(0)
 
 
-def add_rectangular_profile_box_to_face(profiles: List[Any], face):
+def get_profile(frame):
+    profiles = []
+    face_features = {
+        "bbx_frontal_face": [],
+    }
+    face_locations = face_recognition.face_locations(frame)
+    if len(face_locations) > 0:
+        for face_location in face_locations:
+            x0, y1, x1, y0 = face_location
+            bbx_frontal_face = np.array([y0, x0, y1, x1])
+
+            face_features["bbx_frontal_face"] = bbx_frontal_face  # type: ignore
+    profiles.append(face_features)
+    return profiles
+
+
+def add_rectangular_profile_box_to_face(profiles: List[Any], frame):
     for profile in profiles:
         bbx_frontal_face = profile.get("bbx_frontal_face")
         if len(bbx_frontal_face) <= 0:
             continue
         x0, y0, x1, y1 = bbx_frontal_face
-        rectangle_img = cv2.rectangle(face, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        frame = cv2.rectangle(frame, (x0, y0), (x1, y1), (0, 255, 0), 2)
         index = 0
         for key, val in profile.items():
             try:
                 cv2.putText(
-                    rectangle_img,
+                    frame,
                     f"{key}: {val}",
                     (x0, y0 - STEP - 10 * index),
                     cv2.FONT_HERSHEY_SIMPLEX,
@@ -32,7 +50,7 @@ def add_rectangular_profile_box_to_face(profiles: List[Any], face):
             except Exception as e:
                 print(e)
 
-        return rectangle_img
+        return frame
 
 
 while True:
@@ -42,6 +60,9 @@ while True:
 
     end_time = time.time() - start_time
     FPS = 1 / end_time
+
+    profiles = get_profile(frame)
+    annotated_images = add_rectangular_profile_box_to_face(profiles, frame)
 
     cv2.putText(
         frame,
